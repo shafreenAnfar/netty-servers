e4ca58e3253dc5d7b61aa45f35f66d7d40677784
shafreen <shafreen@wso2.com>
2018-04-20 22:27:35 +0530

    Ballerina RPC server

--- src/main/java/carbonReverseProxy/RPCarbonServer.java
+++ src/main/java/carbonReverseProxy/RPCarbonServer.java
@@ -1,18 +1,21 @@
 package carbonReverseProxy;
 
-import org.wso2.carbon.transport.http.netty.config.ListenerConfiguration;
-import org.wso2.carbon.transport.http.netty.config.SenderConfiguration;
-import org.wso2.carbon.transport.http.netty.contract.HttpClientConnector;
-import org.wso2.carbon.transport.http.netty.contract.HttpConnectorListener;
-import org.wso2.carbon.transport.http.netty.contract.HttpResponseFuture;
-import org.wso2.carbon.transport.http.netty.contract.ServerConnector;
-import org.wso2.carbon.transport.http.netty.contract.ServerConnectorException;
-import org.wso2.carbon.transport.http.netty.contract.ServerConnectorFuture;
-import org.wso2.carbon.transport.http.netty.contractimpl.HttpWsConnectorFactoryImpl;
-import org.wso2.carbon.transport.http.netty.listener.ServerBootstrapConfiguration;
-import org.wso2.carbon.transport.http.netty.message.HTTPCarbonMessage;
+import org.wso2.transport.http.netty.config.ListenerConfiguration;
+import org.wso2.transport.http.netty.config.SenderConfiguration;
+import org.wso2.transport.http.netty.contract.HttpClientConnector;
+import org.wso2.transport.http.netty.contract.HttpConnectorListener;
+import org.wso2.transport.http.netty.contract.HttpResponseFuture;
+import org.wso2.transport.http.netty.contract.HttpWsConnectorFactory;
+import org.wso2.transport.http.netty.contract.ServerConnector;
+import org.wso2.transport.http.netty.contract.ServerConnectorException;
+import org.wso2.transport.http.netty.contract.ServerConnectorFuture;
+import org.wso2.transport.http.netty.contractimpl.DefaultHttpWsConnectorFactory;
+import org.wso2.transport.http.netty.listener.ServerBootstrapConfiguration;
+import org.wso2.transport.http.netty.message.HTTPCarbonMessage;
 
 import java.util.HashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * Carbon Server
@@ -20,40 +23,47 @@ import java.util.HashMap;
 public class RPCarbonServer {
     public static void main(String[] args) {
 
-        HttpWsConnectorFactoryImpl httpWsConnectorFactory = new HttpWsConnectorFactoryImpl();
+        final Executor executor = Executors.newFixedThreadPool(100);
 
-        ServerBootstrapConfiguration serverBootstrapConfiguration = ServerBootstrapConfiguration.getInstance();
-        ListenerConfiguration listenerConfiguration = new ListenerConfiguration("xxx", "localhost", 9090);
+        HttpWsConnectorFactory httpWsConnectorFactory = new DefaultHttpWsConnectorFactory(8, 16, 16);
 
-        ServerConnector serverConnector = httpWsConnectorFactory
-                .createServerConnector(serverBootstrapConfiguration, listenerConfiguration);
-        final HttpClientConnector clientConnector = httpWsConnectorFactory
-                .createHttpClientConnector(new HashMap<String, Object>(), new SenderConfiguration());
+        final ServerConnector serverConnector = createServerConnector(httpWsConnectorFactory);
+        final HttpClientConnector clientConnector = createClientConnector(httpWsConnectorFactory);
 
         ServerConnectorFuture serverConnectorFuture = serverConnector.start();
         System.out.println("Server connector started.. ");
         serverConnectorFuture.setHttpConnectorListener(new HttpConnectorListener() {
             @Override
-            public void onMessage(final HTTPCarbonMessage httpRequestMessage) {
-
-                httpRequestMessage.setProperty("HOST", "localhost");
-                httpRequestMessage.setProperty("PORT", 8688);
-                httpRequestMessage.setProperty("HTTP_METHOD", "POST");
-
-                HttpResponseFuture responseFuture = clientConnector.send(httpRequestMessage);
-                responseFuture.setHttpConnectorListener(new HttpConnectorListener() {
+            public void onMessage(final HTTPCarbonMessage inboundRequestMsg) {
+                executor.execute(new Runnable() {
                     @Override
-                    public void onMessage(HTTPCarbonMessage httpCarbonMessage) {
-                        try {
-                            httpRequestMessage.respond(httpCarbonMessage);
-                        } catch (ServerConnectorException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    public void run() {
 
-                    @Override
-                    public void onError(Throwable throwable) {
+                        inboundRequestMsg.setProperty("port", 8688);
+                        inboundRequestMsg.setProperty("host", "127.0.0.1");
 
+                        HttpResponseFuture responseFuture = clientConnector.send(inboundRequestMsg);
+                        responseFuture.setHttpConnectorListener(new HttpConnectorListener() {
+                            @Override
+                            public void onMessage(final HTTPCarbonMessage inboundRespMsg) {
+                                executor.execute(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        try {
+                                            inboundRequestMsg.respond(inboundRespMsg);
+                                        } catch (ServerConnectorException e) {
+                                            e.printStackTrace();
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onError(Throwable throwable) {
+                            }
+                        });
                     }
                 });
             }
@@ -63,11 +73,20 @@ public class RPCarbonServer {
 
             }
         });
+    }
 
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private static HttpClientConnector createClientConnector(HttpWsConnectorFactory httpWsConnectorFactory) {
+        SenderConfiguration senderConfiguration = new SenderConfiguration();
+        senderConfiguration.setSocketIdleTimeout(30000);
+        return httpWsConnectorFactory
+                .createHttpClientConnector(new HashMap<String, Object>(), senderConfiguration);
+    }
+
+    private static ServerConnector createServerConnector(HttpWsConnectorFactory httpWsConnectorFactory) {
+        ServerBootstrapConfiguration serverBootstrapConfiguration =
+                new ServerBootstrapConfiguration(new HashMap<String, Object>());
+        ListenerConfiguration listenerConfiguration = new ListenerConfiguration("xxx", "localhost", 9090);
+        return httpWsConnectorFactory
+                .createServerConnector(serverBootstrapConfiguration, listenerConfiguration);
     }
 }
